6de9ac73d0400a77ac36899047c20903bb4292bc
shikharmaxx <shikharmalik2010@gmail.com>
2020-07-27 18:03:37 +0530

    JS dom part 1 completed

--- JS dom crash course/crash course 1/dom.js
+++ JS dom crash course/crash course 1/dom.js
@@ -37,4 +37,114 @@
 
 // console.log(document.links);
 
-console.log(document.images);
+//console.log(document.images);
+
+// GET ELEMENT BY ID //
+//console.log(document.getElementById("header-title"));
+// here we selected the header with its html id name
+
+//same as above just a different way to do it
+//var headerTitle = document.getElementById("header-title");
+// console.log(headerTitle);
+
+// //headerTitle.textContent = "hello";
+// //changes the heading to "hello"
+
+// //headerTitle.innerText = "goodbye";
+// //does the same thing as above but there is a difffernece
+
+// console.log(headerTitle.textContent);
+// //displays item lister and 123 which is in span in header
+
+// console.log(headerTitle.innerText);
+// // the 123 is not there this pays attention to the styling
+
+// // but we can usuauly use both as they have the same fucntion
+
+// headerTitle.innerHTML = "<h3>this is h3</h3>";
+// //this puts it inside the originial header
+// //doesnt change it to h3 tag just puts it in there
+
+// headerTitle.style.borderBottom = "solid 3px black";
+// // so this puts bottom under the header titile
+
+// //if we want it to go on the actual header
+// header = document.getElementById("main-header");
+// header.style.borderBottom = "solid 5px hotpink";
+
+// GET ELEMENTS BY CLASS NAME //
+
+//we know list items have a class
+// var items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+
+// //for a particular item
+// console.log(items[1]);
+
+// //changing the text content
+// items[1].textContent = "Hello helo";
+// //lets change the style too
+// items[1].style.fontWeight = "bold";
+// items[1].style.backgroundColor = "yellow";
+
+// //For applying to all the list items at once
+// //items.style.backgroundColor ='#f4f4f4';
+// //this is not gonna work , this will give error
+// // we have to actually loop through eacch of them
+// for (var i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = "#f4f4";
+// }
+
+// // SET ELEMENTS BY TAG NAME //
+
+// var li = document.getElementsByTagName("li");
+// console.log(li);
+// console.log(li[2]);
+// li[2].style.backgroundColor = "lime";
+
+// for (var i = 0; i < li.length; i++) {
+//   li[i].style.backgroundColor = "cyan";
+// }
+// // where this comes in handy is if we add a new list item
+// // without the list class
+// // with get by tag name we can still apply the same changes to the new item
+
+// //  QUERY SELECTOR //
+// var header = document.querySelector("#main-header");
+// header.style.borderBottom = "solid 4px cyan";
+
+// var input = document.querySelector("input");
+// input.value = "hello world";
+// //this adds the value to the form input box
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = "send";
+// // this changs the text inside the button
+// //replaces it from submit to send
+
+// var item = document.querySelector(".list-group-item");
+// item.style.color = "red";
+
+// var lastitem = document.querySelector(".list-group-item:last-child");
+// lastitem.style.color = "hotpink";
+// // selecting the last element from the list items
+
+// var lastitem = document.querySelector(".list-group-item:nth-child(2)");
+// lastitem.style.color = "coral";
+// //this way we can select the nth child
+
+//  QUERY SELECTOR ALL //
+
+//used to grab more than one thing
+var titles = document.querySelectorAll(".title");
+console.log(titles);
+titles[0].textContent = "hello bruh";
+
+//to change multiple properties
+var odd = document.querySelectorAll("li:nth-child(odd)");
+var even = document.querySelectorAll("li:nth-child(even)");
+
+for (var i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "red";
+  even[i].style.backgroundColor = "coral";
+}
